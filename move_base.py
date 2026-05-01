b6615dc66ccb59eb7bd3c55fe8f311953d25a8f4
3034681763-de <3034681763@qq.com>
2026-05-01 16:17:09 +0800

    fix: relax tracking alignment stop behavior

--- move_base.py
+++ move_base.py
@@ -660,7 +660,7 @@ class Task_func:
                         debug=debug,
                         missed_frames=missed_frames,
                     )
-                self.base.MOD_ALIGN_DONE()
+                self.base.MOD_STOP()
                 self.tracking_aligner.reset()
                 print("[Tracking] Location OK")
                 return True
